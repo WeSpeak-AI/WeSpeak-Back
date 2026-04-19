56023df6c17203d3c4db424bfab3c14b39b854fd
jeongseonwoong <tjsdnd1219@naver.com>
2026-04-19 15:43:56 +0900

    feat: 시험용 엔티티

--- WeSpeak-Back/core/core-domain/src/main/java/backend/core/domain/test/IncorrectWord.java
+++ WeSpeak-Back/core/core-domain/src/main/java/backend/core/domain/test/IncorrectWord.java
@@ -1,2 +1,25 @@
-package backend.core.domain.test;public class IncorrectWord {
+package backend.core.domain.test;
+
+import backend.core.domain.voca.Word;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "incorrect_word")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@AllArgsConstructor
+public class IncorrectWord {
+
+    @Id
+    private Long incorrectWordId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "test_id")
+    private Test test;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "word_id")
+    private Word word;
 }
--- WeSpeak-Back/core/core-domain/src/main/java/backend/core/domain/test/Test.java
+++ WeSpeak-Back/core/core-domain/src/main/java/backend/core/domain/test/Test.java
@@ -1,2 +1,32 @@
-package backend.core.domain.test;public class Test {
+package backend.core.domain.test;
+
+import backend.core.domain.user.User;
+import backend.core.domain.voca.VocaBook;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "test")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@AllArgsConstructor
+public class Test {
+
+    @Id
+    private Long testId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "voca_book_id")
+    private VocaBook vocaBook;
+
+    @Column(nullable = false)
+    private int startDay;
+
+    @Column(nullable = false)
+    private int endDay;
 }
