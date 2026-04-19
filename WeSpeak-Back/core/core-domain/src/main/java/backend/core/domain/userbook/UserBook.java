2132962c175d5f2bf3cb89477204322f6091a99f
jeongseonwoong <tjsdnd1219@naver.com>
2026-04-19 15:44:33 +0900

    feat: 사용자 책 엔티티

--- WeSpeak-Back/core/core-domain/src/main/java/backend/core/domain/userbook/UserBook.java
+++ WeSpeak-Back/core/core-domain/src/main/java/backend/core/domain/userbook/UserBook.java
@@ -1,2 +1,26 @@
-package backend.core.domain.userbook;public class UserBook {
+package backend.core.domain.userbook;
+
+import backend.core.domain.reading.Book;
+import backend.core.domain.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "user_book")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@AllArgsConstructor
+public class UserBook {
+
+    @Id
+    private Long userBookId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_id")
+    private Book book;
 }
