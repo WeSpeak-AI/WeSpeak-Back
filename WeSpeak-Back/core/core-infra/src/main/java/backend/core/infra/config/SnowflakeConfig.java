218d0313f0540c255f280c8dbe0c594ada6ffb83
jeongseonwoong <tjsdnd1219@naver.com>
2026-04-19 15:46:56 +0900

    feat: Snowflake 설정 구현

--- WeSpeak-Back/core/core-infra/src/main/java/backend/core/infra/config/SnowflakeConfig.java
+++ WeSpeak-Back/core/core-infra/src/main/java/backend/core/infra/config/SnowflakeConfig.java
@@ -1,2 +1,14 @@
-package backend.core.infra.config;public class SnowflakeConfig {
+package backend.core.infra.config;
+
+import backend.core.infra.Snowflake;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SnowflakeConfig {
+
+    @Bean
+    public Snowflake snowflake() {
+        return new Snowflake();
+    }
 }
