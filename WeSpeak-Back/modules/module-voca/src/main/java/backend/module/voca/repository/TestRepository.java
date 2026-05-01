737a9c869f62e279e8385940e2813d84abe50e5e
jeongseonwoong <tjsdnd1219@naver.com>
2026-05-01 15:48:43 +0900

    feat: 전용 조회 메서드 추가

--- WeSpeak-Back/modules/module-voca/src/main/java/backend/module/voca/repository/TestRepository.java
+++ WeSpeak-Back/modules/module-voca/src/main/java/backend/module/voca/repository/TestRepository.java
@@ -6,9 +6,13 @@ import backend.core.domain.voca.VocaBook;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TestRepository extends JpaRepository<Test, Long> {
 
     List<Test> findByUserAndVocaBook(User user, VocaBook vocaBook);
 
+    List<Test> findByUserEmailAndVocaBookVocaBookId(String email, Long vocaBookId);
+
+    Optional<Test> findByTestIdAndUserEmail(Long testId, String email);
 }
