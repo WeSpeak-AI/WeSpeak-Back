e992c2e26dc4ad6c798c20ea9f4c7000013d1c73
jeongseonwoong <tjsdnd1219@naver.com>
2026-05-01 15:49:06 +0900

    feat: 전용 조회메서드 추가

--- WeSpeak-Back/modules/module-voca/src/main/java/backend/module/voca/repository/IncorrectWordRepository.java
+++ WeSpeak-Back/modules/module-voca/src/main/java/backend/module/voca/repository/IncorrectWordRepository.java
@@ -3,11 +3,17 @@ package backend.module.voca.repository;
 
 import backend.core.domain.test.IncorrectWord;
 import backend.core.domain.test.Test;
-import backend.core.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface IncorrectWordRepository extends JpaRepository<IncorrectWord, Long> {
     List<IncorrectWord> findByTest(Test test);
+
+    @Modifying
+    @Query("delete from IncorrectWord iw where iw.test.testId = :testId")
+    void deleteByTestId(@Param("testId") Long testId);
 }
