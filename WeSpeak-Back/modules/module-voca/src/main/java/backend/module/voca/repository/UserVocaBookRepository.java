ea155c5808c6696f4ea0fd81247408ee6b66474f
jeongseonwoong <tjsdnd1219@naver.com>
2026-05-01 15:48:21 +0900

    feat: 전용 조회 메서드 구현

--- WeSpeak-Back/modules/module-voca/src/main/java/backend/module/voca/repository/UserVocaBookRepository.java
+++ WeSpeak-Back/modules/module-voca/src/main/java/backend/module/voca/repository/UserVocaBookRepository.java
@@ -4,14 +4,24 @@ import backend.core.domain.user.User;
 import backend.core.domain.uservoca.UserVocaBook;
 import backend.core.domain.voca.VocaBook;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserVocaBookRepository extends JpaRepository<UserVocaBook, Long> {
 
     boolean existsByUserAndVocaBook(User user, VocaBook vocaBook);
 
+    boolean existsByUserEmailAndVocaBookVocaBookId(String email, Long bookId);
+
     List<UserVocaBook> findByUser(User user);
 
-    java.util.Optional<UserVocaBook> findByUserAndVocaBook(User user, VocaBook vocaBook);
+    @Query("select uv from UserVocaBook uv join fetch uv.vocaBook where uv.user.email = :email")
+    List<UserVocaBook> findByUserEmail(@Param("email") String email);
+
+    Optional<UserVocaBook> findByUserAndVocaBook(User user, VocaBook vocaBook);
+
+    Optional<UserVocaBook> findByUserEmailAndVocaBookVocaBookId(String email, Long bookId);
 }
