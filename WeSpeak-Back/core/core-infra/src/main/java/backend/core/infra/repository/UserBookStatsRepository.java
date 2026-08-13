caa81d9bb1f45c13fa7773cbb610792fb5f0beeb
jeongseonwoong <tjsdnd1219@naver.com>
2026-08-13 13:13:46 +0900

    모듈 별 DB 분리

--- WeSpeak-Back/core/core-infra/src/main/java/backend/core/infra/repository/UserBookStatsRepository.java
+++
@@ -1,8 +0,0 @@
-package backend.core.infra.repository;
-
-import backend.core.domain.userbook.UserBook;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserBookStatsRepository extends JpaRepository<UserBook, Long> {
-    long countByUserEmail(String email);
-}
