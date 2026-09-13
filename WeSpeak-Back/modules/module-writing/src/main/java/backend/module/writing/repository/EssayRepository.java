3baf58ce3699e0dba6b01ab3035c963c4c28bcf4
jeongseonwoong <tjsdnd1219@naver.com>
2026-09-13 12:15:25 +0900

    feat: Essay 현재 상태를 기반으로 조회하는 기능 추가

--- WeSpeak-Back/modules/module-writing/src/main/java/backend/module/writing/repository/EssayRepository.java
+++ WeSpeak-Back/modules/module-writing/src/main/java/backend/module/writing/repository/EssayRepository.java
@@ -1,6 +1,8 @@
 package backend.module.writing.repository;
 
+import backend.module.writing.domain.CorrectionStatus;
 import backend.module.writing.domain.Essay;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,4 +10,6 @@ import java.util.List;
 public interface EssayRepository extends JpaRepository<Essay, Long> {
 
     List<Essay> findByUserEmailOrderByCreatedAtDesc(String email);
+
+    List<Essay> findByCorrectionStatusOrderByCreatedAtAsc(CorrectionStatus correctionStatus, Pageable pageable);
 }
