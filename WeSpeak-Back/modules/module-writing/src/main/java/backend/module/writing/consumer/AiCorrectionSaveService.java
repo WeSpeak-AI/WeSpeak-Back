a819d686a6148570f59c5fbd0aaa6a28cb718389
jeongseonwoong <tjsdnd1219@naver.com>
2026-09-13 12:09:11 +0900

    feat: AI 교정 DLT에서 상태를 FAILED로 변경 만약 에세이가 이미 삭제된 상태이거나, 완료된 경우 종료

--- WeSpeak-Back/modules/module-writing/src/main/java/backend/module/writing/consumer/AiCorrectionSaveService.java
+++ WeSpeak-Back/modules/module-writing/src/main/java/backend/module/writing/consumer/AiCorrectionSaveService.java
@@ -2,4 +2,6 @@ package backend.module.writing.consumer;
 
 public interface AiCorrectionSaveService {
     void applyAiCorrection(Long essayId, String correctionResult);
+
+    void markCorrectionFailed(Long essayId);
 }
